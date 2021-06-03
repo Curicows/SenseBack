509ff9bdd4fab80e8eeb3ee559df8426d0373c73
Giovane Marcelo <giovane0845@gmail.com>
2021-06-03 04:35:14 -0300

    CORS

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\TipoController;
+use App\Http\Controllers\TransacaoController;
+
+Route::resource('categoria', CategoriaController::class);
+
+Route::get('tipo',[TipoController::class, 'index']);
+
+Route::resource('transacao', TransacaoController::class);
